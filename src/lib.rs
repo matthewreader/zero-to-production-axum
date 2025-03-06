993900720a1984187a955a83422950c27291709e
Matthew Reader <mdreader@gmail.com>
2025-03-06 10:39:51 -0600

    Refactor code to facilitate testing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,36 @@
-use axum::{Json, Router};
-use axum::routing::get;
+use axum::{routing::get, Json, Router};
+use axum::http::StatusCode;
+use tokio::net::TcpListener;
+use axum::response::IntoResponse;
 use chrono::Utc;
 use serde_json::{json, Value};
+use tracing::{info, error};
+use tracing_subscriber;
 
-pub async fn run() {
+pub fn run(listener: TcpListener) -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt::init();
+
+    let addr = listener.local_addr().expect("could not get listener addr");
+    info!("Server is running on: {}", addr);
+
     let app = Router::new()
         .route("/health", get(health_check));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .expect("Failed to bind to address");
+    let server = axum::serve(listener, app.into_make_service());
 
-    println!("Listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app.into_make_service())
-        .await
-        .expect("Failed to run server");
+    tokio::spawn(async move {
+        if let Err(e) = server.await {
+            error!("server error: {}", e);
+        }
+    });
+
+    Ok(())
 }
-async fn health_check() -> Json<Value> {
+async fn health_check() -> impl IntoResponse {
     let dtm = Utc::now();
     let response: Value = json!({
         "status": 200,
         "time": dtm
     });
-    Json(response)
+    (StatusCode::OK, Json(response))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use zero_to_production_axum::run;
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    run().await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8000").await?;
+    run(listener)?;
+
+    tokio::signal::ctrl_c().await?;
+    Ok(())
 }
